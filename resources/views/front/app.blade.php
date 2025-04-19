121ba6e154ed070d2018114f8b73e697db3e2281
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-04-19 21:18:10 +0700

    fix: favicon front

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -16,10 +16,8 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ Storage::url($setting_web->favicon) }}">
 
-
-    <!-- Place favicon.png in the root directory -->
-    <link rel="shortcut icon" href="{{ asset('front/img/favicon.png') }}" type="image/x-icon" />
     <!-- Font Icons css -->
     <link rel="stylesheet" href="{{ asset('front/css/font-icons.css') }}">
     <!-- plugins css -->
