dafbb2d5ef83ac4af2e08420ba5a48a475f2508a
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-09-12 09:12:12 +0700

    refactor: whatsapp message

--- resources/views/back/pages/whatsapp/message/send-image.blade.php
+++ resources/views/back/pages/whatsapp/message/send-image.blade.php
@@ -16,11 +16,12 @@
                 <div class="card-toolbar">
                     <ul class="nav nav-tabs nav-line-tabs nav-stretch fs-6 border-0">
                         <li class="nav-item">
-                            <a class="nav-link active" data-bs-toggle="tab" href="#kt_tab_pane_7">No Whatsapp</a>
+                            <a class="nav-link active" data-bs-toggle="tab" href="#kt_tab_pane_8">Pengguna</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-bs-toggle="tab" href="#kt_tab_pane_8">Pengguna</a>
+                            <a class="nav-link " data-bs-toggle="tab" href="#kt_tab_pane_7">No Whatsapp</a>
                         </li>
+
                     </ul>
                 </div>
             </div>
@@ -28,7 +29,58 @@
 
             <div class="card-body ">
                 <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="kt_tab_pane_7" role="tabpanel">
+                    <div class="tab-pane fade show active" id="kt_tab_pane_8" role="tabpanel">
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="{{ asset('ext_images/wa_massage.png') }}" style="width: 100%" alt="" />
+                            </div>
+                            <div class="col-md-8">
+                                <form id="kt_modal_create_discipline_rule_form" class="form" method="POST"
+                                    action="{{ route('back.whatsapp.message.sendImageProcess') }}"
+                                    enctype="multipart/form-data">
+                                    @csrf
+
+                                    <div class="fv-row mb-10">
+                                        <label class="required fw-bold fs-6 mb-2">Pengguna</label>
+                                        <select id="kt_docs_select2_rich_content" class="form-select form-select-solid"
+                                            name="phone" data-placeholder="Pilih Pengguna" required>
+                                            @foreach ($users as $user)
+                                                <option></option>
+                                                <option value="{{ $user->phone }}"
+                                                    data-kt-rich-content-subcontent="Telp. {{ $user->phone }}"
+                                                    data-kt-rich-content-icon="{{ $user->getPhoto() }}">
+                                                    {{ $user->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="fv-row mb-10">
+                                        <label class="required fw-bold fs-6 mb-2">Gambar</label>
+                                        <input type="file"
+                                            class="form-control form-control-solid form-control-lg fw-bold" name="image"
+                                            accept="image/*" required />
+                                        <small class="text-muted">Pastikan gambar yang diunggah berformat <code>jpg</code>,
+                                            <code>jpeg</code>, atau <code>png</code>.</small>
+                                    </div>
+
+                                    <div class="fv-row mb-10">
+                                        <label class="required fw-bold fs-6 mb-2">Pesan</label>
+                                        <textarea class="form-control form-control-solid form-control-lg fw-bold" name="message" rows="10" placeholder=""></textarea>
+                                    </div>
+
+                                    <div class="d-flex justify-content-end">
+                                        <button type="submit" class="btn btn-primary" id="send_message_btn">
+                                            <span class="indicator-label">Kirim Pesan</span>
+                                        </button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade " id="kt_tab_pane_7" role="tabpanel">
                         <div class="row">
                             <div class="col-md-4">
                                 <img src="{{ asset('ext_images/wa_massage.png') }}" style="width: 100%" alt="" />
@@ -72,56 +124,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane fade" id="kt_tab_pane_8" role="tabpanel">
 
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="{{ asset('ext_images/wa_massage.png') }}" style="width: 100%" alt="" />
-                        </div>
-                        <div class="col-md-8">
-                            <form id="kt_modal_create_discipline_rule_form" class="form" method="POST"
-                                action="{{ route('back.whatsapp.message.sendImageProcess') }}"
-                                enctype="multipart/form-data">
-                                @csrf
-
-                                <div class="fv-row mb-10">
-                                    <label class="required fw-bold fs-6 mb-2">Pengguna</label>
-                                    <select id="kt_docs_select2_rich_content" class="form-select form-select-solid"
-                                        name="phone" data-placeholder="Pilih Pengguna" required>
-                                        @foreach ($users as $user)
-                                            <option></option>
-                                            <option value="{{ $user->phone }}"
-                                                data-kt-rich-content-subcontent="Telp. {{ $user->phone }}"
-                                                data-kt-rich-content-icon="{{ $user->getPhoto() }}">
-                                                {{ $user->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="fv-row mb-10">
-                                    <label class="required fw-bold fs-6 mb-2">Gambar</label>
-                                    <input type="file" class="form-control form-control-solid form-control-lg fw-bold"
-                                        name="image" accept="image/*" required />
-                                    <small class="text-muted">Pastikan gambar yang diunggah berformat <code>jpg</code>,
-                                        <code>jpeg</code>, atau <code>png</code>.</small>
-                                </div>
-
-                                <div class="fv-row mb-10">
-                                    <label class="required fw-bold fs-6 mb-2">Pesan</label>
-                                    <textarea class="form-control form-control-solid form-control-lg fw-bold" name="message" rows="10" placeholder=""></textarea>
-                                </div>
-
-                                <div class="d-flex justify-content-end">
-                                    <button type="submit" class="btn btn-primary" id="send_message_btn">
-                                        <span class="indicator-label">Kirim Pesan</span>
-                                    </button>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
-                    </div>
 
                 </div>
 
