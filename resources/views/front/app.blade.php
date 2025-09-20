62f72506cb0c823b1d06ef7dc8a951308563798c
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-09-20 17:31:08 +0700

    fix: issue select on sweetalert

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
-<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}">
+<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}">
 
 @php
     $setting_web = \App\Models\SettingWebsite::first();
@@ -26,10 +27,15 @@
     <link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
     <!-- Responsive css -->
     <link rel="stylesheet" href="{{ asset('front/css/responsive.css') }}">
-    @if(app()->getLocale() === 'ar' && file_exists(public_path('front/css/rtl.css')))
+    @if (app()->getLocale() === 'ar' && file_exists(public_path('front/css/rtl.css')))
         <link rel="stylesheet" href="{{ asset('front/css/rtl.css') }}">
     @endif
     @yield('styles')
+    <style>
+        .swal2-select {
+            display: none !important;
+        }
+    </style>
 </head>
 
 <body class="{{ app()->getLocale() === 'ar' ? 'rtl' : '' }}">
